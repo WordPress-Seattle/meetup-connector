b27e3043cf352e55f96bc8c2fca6f3d4a357a40e
Will Anderson <will@itsananderson.com>
2013-03-27 02:11:12 -0700

    Initial stub

---
+++ meetup-connector.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Plugin Name: Meetup Connector
+ * Description: Connect user accounts to Meetup.com
+ * Plugin URI: http://github.com/WordPress-Seattle/meetup-connector
+ * Author Name: WordPress Seattle
+ * Author URI: http://wpseattle.org/
+ * Version: 0.1
+ */
+
+class Meetup_Connector {
+    public static function start() {
+
+    }
+}
+
+Meetup_Connector::start();
